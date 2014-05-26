4f0abd9a6783895c404f995cb9ba549e6e4b4cd0
Saji Hameed <saji.nh@gmail.com>
2014-05-26 15:08:37 +0900

    synthetic example for demo

---
+++ examples/synthetic/som_input.rb
@@ -0,0 +1,25 @@
+require 'numru/netcdf'
+require 'pp'
+include NumRu
+
+file = NetCDF.open("synth.nc")
+rain = file.var("rain").get
+station = file.var("station").get
+file.close
+
+pp rain.class
+pp rain.size
+pp rain.dim
+pp rain.shape
+ntim,nstn = rain.shape
+
+# we need only rain for selected season
+
+fout = File.open("synth.txt","w")
+fout.puts "#{ntim} rect 2 2 gaussian"
+(0..nstn-1).each do |istn|
+  txt = rain[true,istn]
+  fout.puts txt.to_a.map{|f| "% 2.2f"%f}.join(" ")+" #{"%02d" % station[istn]}"
+end
+fout.close
+
